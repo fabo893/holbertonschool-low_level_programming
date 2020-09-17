f5ac915d26103956182d6faa046fa7fdb29ba18f
fabo893 <jose.rosa893@gmail.com>
2020-09-17 18:41:46 +0000

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,15 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (sucess)
  */
 int main(void)
 {
-printf("Size of a char: %lu bytes(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu bytes(s)\n", sizeof(long));
-printf("Size of a long long: %lu byte(s)\n", sizeof(long long));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
+printf("Size of a char: %d bytes(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d bytes(s)\n", sizeof(long));
+printf("Size of a long long: %d byte(s)\n", sizeof(long long));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
