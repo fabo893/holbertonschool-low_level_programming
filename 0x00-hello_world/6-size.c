9381b6aaf29a236b3ef4cb60ee47eefcc8d700dc
fabo893 <jose.rosa893@gmail.com>
2020-09-18 15:54:47 +0000

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,6 @@ printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long));
 printf("Size of a long long: %lu byte(s)\n", sizeof(long long));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 return (0);
 }
