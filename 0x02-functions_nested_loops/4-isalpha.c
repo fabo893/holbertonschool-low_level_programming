471ac8a9441beb64ce4eba665562c76b6a583278
fabo893 <jose.rosa893@gmail.com>
2020-09-23 03:02:06 +0000

    Is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Check for alphabet character
+ * @c: int
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
