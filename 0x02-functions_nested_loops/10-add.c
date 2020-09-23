4f3b19f9c62cd8e7b206c5ce063ba779c47c5ed5
fabo893 <jose.rosa893@gmail.com>
2020-09-23 03:40:37 +0000

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - add 2 int
+ * @x; first int parameter
+ * @y: second int parameter
+ * Return: z int addition
+ */
+int ad(int x, int y)
+{
+	int i = x + y;
+
+	return (i);
+}
