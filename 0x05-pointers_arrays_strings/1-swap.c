19782356a97c43465e044c1231772eea22a6c945
fabo893 <jose.rosa893@gmail.com>
2020-09-29 22:28:01 +0000

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap - swap the values of two integers
+ * @a: first param
+ * @b: second param
+ * Return: always zero
+ */
+void swap_int(int *a, int *b)
+{
+	int c = 98;
+
+	*a = *b;
+	*b = c;
+}
