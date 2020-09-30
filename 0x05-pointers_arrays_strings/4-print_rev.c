bf30014d2275878c862d633f8d36ed4bd6dc2c96
fabo893 <jose.rosa893@gmail.com>
2020-09-30 03:57:30 +0000

    Print Reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,14 +3,20 @@
 /**
  * print_rev - prints a string in reverse
  * @s: parameter
+ * _strlen - prints length
  */
 void print_rev(char *s)
 {
-	int x;
+	int x,y;
 
-	for (x = 64; s[x] >= '\0'; x--)
+	for (x = 0; s[x] != '\0'; )
 	{
-		_putchar(s[x]);
+		x++;
+	}
+
+	for (y = x - 1; y >= 0; y--)
+	{
+		_putchar(*(s + y));
 	}
 	_putchar('\n');
 }
