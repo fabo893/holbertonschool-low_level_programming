6525fd880b88e600425aa061c3ae54d73c56e693
fabo893 <jose.rosa893@gmail.com>
2020-10-01 18:08:22 +0000

    Puts Half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * puts_half -prints half of a string
+ * @str: parameter
+ */
+void puts_half(char *str)
+{
+	int x, y, n;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		;
+	}
+	if (x % 2 == 0)
+	{
+		for (y = x / 2; str[y] != '\0'; y++)
+			_putchar(str[y]);
+	}
+	else
+	{
+		for (n = (x - 1) / 2; str[n] != '\0'; n++)
+			_putchar(str[n]);
+	}
+	_putchar('\n');
+}
