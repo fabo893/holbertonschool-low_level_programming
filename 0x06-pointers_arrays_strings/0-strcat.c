aaa07be1f9b7030f3f37796266e4d9c39c5fae9f
fabo893 <jose.rosa893@gmail.com>
2020-10-03 04:50:47 +0000

    String cat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strcat: pointer
+ * @dest: first parameter
+ * @src: second parameter
+ * Return: return a pointer to 'dest'
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	for (x = 0; dest[x] != '\0'; x++)
+		;
+	for (y = 0; src[y] != '\0'; y++)
+	{
+		dest[x + y] = src[y];
+	}
+	return (dest);
+}
