b27e9e1851d0cc3a6c646ab6d7532b7b7f794904
fabo893 <jose.rosa893@gmail.com>
2020-10-08 16:21:24 +0000

    String copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copy a string
+ * @dest: variable char
+ * @src: variable char
+ * @n: variable int
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; src[x] != '\0' && x < n; x++)
+	{
+		dest[x] = src[x];
+	}
+
+	for (x; x < n; x++)
+	{
+		dest[x] = '\0';
+	}
+	return (dest);
+}
