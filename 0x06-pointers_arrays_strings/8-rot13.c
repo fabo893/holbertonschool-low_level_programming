26144114cd6a1060956d9dc31eb1923c465f9f0e
fabo893 <jose.rosa893@gmail.com>
2020-10-08 17:59:49 +0000

    Encoding rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * *rot13 -  encodes a string using.
+ * @s: pointer
+ * Return: 0
+ */
+char *rot13(char *s)
+{
+	int c, i;
+	char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char n[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (c = 0; s[c] != '\0'; c++)
+		for (i = 0; l[i] != '\0'; i++)
+			if (s[c] == l[i])
+			{
+				s[c] = n[i];
+				break;
+			}
+	return (s);
+}
