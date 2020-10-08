252ecde54d43899a3a76a23d43004c098897a7d4
fabo893 <jose.rosa893@gmail.com>
2020-10-08 15:03:18 +0000

    Puts 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
 
 /**
- * puts2 - print every other character of a string
- * @str: parameter
+ * puts2 - Prints a string in reverse
+ * @str: char
  */
+
 void puts2(char *str)
 {
 	int x;
 
-	for (x = 0; str[x] != '\0'; x += 2)
+	for (x = 0; str[x] != '\0'; x++)
 	{
-		_putchar(str[x]);
+		if (x % 2 == 0)
+			_putchar(*(str + x));
 	}
 	_putchar('\n');
 }
