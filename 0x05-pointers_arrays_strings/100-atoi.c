38238ea3455f4c0e851344032321fecee75fbf49
fabo893 <jose.rosa893@gmail.com>
2020-10-08 15:26:16 +0000

    Atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _atoi - Function that convert string to int
+ * @s: String to convert to int
+ *
+ * Return: Value of converted number
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, convert = 0;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		convert = convert * 10 + s[i] - '0';
+	}
+
+	if (*s == '\0')
+		return (0);
+
+	return (convert);
+}
