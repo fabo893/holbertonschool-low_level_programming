49d68fa1a6e7e6b2e8e976e26ddaa97e187335e6
fabo893 <jose.rosa893@gmail.com>
2020-10-17 04:03:20 +0000

    Fixed code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,25 +6,18 @@
  */
 void puts_half(char *str)
 {
-	int length_of_the_string, y, n;
+	int length_of_the_string, n;
 
 	for (length_of_the_string = 0; str[length_of_the_string] != '\0'; )
 	{
 		length_of_the_string++;
 	}
-	if (length_of_the_string % 2 == 0)
+
+	length_of_the_string += 1;
+
+	for (n = length_of_the_string / 2; str[n] != '\0'; n++)
 	{
-		for (y = length_of_the_string / 2; str[y] != '\0'; y++)
-		{
-			_putchar(str[y]);
-		}
-	}
-	else
-	{
-		for (n = (length_of_the_string - 1) / 2; str[n] != '\0'; n++)
-		{
-			_putchar(str[n]);
-		}
+		_putchar(str[n]);
 	}
 	_putchar('\n');
 }
