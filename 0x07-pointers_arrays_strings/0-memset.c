8c7821305df85ad88686a210ca905c00bec69e2e
fabo893 <jose.rosa893@gmail.com>
2020-10-17 21:02:49 +0000

    memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * *_memset - fill memory
+ * @s: pointer
+ * @b: variable
+ * @n: integer
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x <= n - 1; x++)
+	{
+		s[x] = b;
+	}
+	return(s);
+}
