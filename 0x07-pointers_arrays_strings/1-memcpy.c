5bd51e54805a05e1aafcf1fe776d8f1d508b1bfd
fabo893 <jose.rosa893@gmail.com>
2020-10-18 00:12:24 +0000

    memcpy function

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * *_memcpy - copy the memory area
+ * @dest: pointer
+ * @src: pointer
+ * @n: unsigned integer
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		dest[x] = src[x];
+	}
+	return(dest);
+}
