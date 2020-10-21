9ae18925eb04113f19a8723727ffd38bc5eba0c4
fabo893 <jose.rosa893@gmail.com>
2020-10-21 07:02:36 +0000

    ArrayIterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a funciton given
+ * @array: array
+ * @size: size of the array
+ * @action: pointer to function
+ * @int: integer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+  unsigned int x;
+
+  for (x = 0; x < size; x++)
+    {
+      action(array[x]);
+    }
+}
