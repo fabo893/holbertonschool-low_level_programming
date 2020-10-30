30e617c1dff26c30f16a79a88fce517205141461
fabo893 <jose.rosa893@gmail.com>
2020-10-30 06:59:13 +0000

    Whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - prints its name
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: On sucess always 0
+ */
+int main(int argc, char* argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+
+	return (0);
+}
