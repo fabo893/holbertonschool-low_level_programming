3d0143fa9e169e27dc1eb9816951d8c7ab7124bc
fabo893 <jose.rosa893@gmail.com>
2020-10-30 07:30:45 +0000

    Args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - Prints the number of arguments passed into it
+ * @argc: Arguments count
+ * @argc: Arguments array
+ *
+ * Return: On success always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
