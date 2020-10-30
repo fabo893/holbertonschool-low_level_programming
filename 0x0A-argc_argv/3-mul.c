d21c8b53bf5240b2ea573cd91077be4c151b59f5
fabo893 <jose.rosa893@gmail.com>
2020-10-30 08:56:06 +0000

    Mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * main - Multiply two numbers
+ * @argc: Arguments count
+ * @argv: Arguments array
+ *
+ * Return: On success, always 0
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", mul);
+	}
+
+
+	return (0);
+}
