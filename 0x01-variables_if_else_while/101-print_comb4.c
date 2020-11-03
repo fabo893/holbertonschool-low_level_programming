1a1381f86c5ce4801c49e3d9a4e56047a3bde348
fabo893 <jose.rosa893@gmail.com>
2020-11-03 20:35:19 +0000

    Print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,7 +4,6 @@
  * main - print all possible combinations
  * Return: On success, always 0
  */
-<<<<<<< HEAD
 int main(void)
 {
 	int i, x, y;
@@ -31,32 +30,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-=======
-int main(void) {
-
-  int i, x, y;
-
-  for (i = 0; i <= 9; i++)
-    {
-      for (x = 0; x <= 9; x++)
-	{
-	  for (y = 0; y <= 9; y++)
-	    {
-	      if (i != x && x != y && y != i && i < x && x < y)
-		{
-		  putchar(i + '0');
-		  putchar(x + '0');
-		  putchar(y + '0');
-		  if (i != 7 || x != 8 || y != 9)
-		    {
-		      putchar(',');
-		      putchar(' ');
-		    }
-		}
-	    }
-	}
-    }
-  putchar('\n');
-  return 0;
->>>>>>> ff946957170de227274334ec61e624618da5a266
 }
