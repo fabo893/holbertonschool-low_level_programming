27215b105357cb6c6bb58ded40f0ce3784652d43
fabo893 <jose.rosa893@gmail.com>
2020-11-03 04:07:54 +0000

    Print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+  int i, x, y;
+
+  for (i = 0; i <= 9; i++)
+    {
+      for (x = 0; x <= 9; x++)
+	{
+	  for (y = 0; y <= 9; y++)
+	    {
+	      if (i != x && x != y && y != i && i < x && x < y)
+		{
+		  putchar(i + '0');
+		  putchar(x + '0');
+		  putchar(y + '0');
+		  if (i != 7 || x != 8 || y != 9)
+		    {
+		      putchar(',');
+		      putchar(' ');
+		    }
+		}
+	    }
+	}
+    }
+  putchar('\n');
+  return 0;
+}
