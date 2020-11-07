404c5bea610677a4c860dfdc78edf4d899ae7623
fabo893 <jose.rosa893@gmail.com>
2020-11-07 02:15:47 +0000

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - return the number of bits you need to flip number to other number
+ * @n: a given number to change
+ * @m: number expected after the change
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int bits = 0;
+	int xor;
+
+	for (xor = n ^ m; xor != 0; xor = xor >> 1)
+	{
+		bits += xor & 1;
+	}
+	return (bits);
+
+}
