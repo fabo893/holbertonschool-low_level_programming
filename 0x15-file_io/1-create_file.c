22a2cdce0940ff49b9d385baf5bb246abd384dd2
fabo893 <jose.rosa893@gmail.com>
2020-11-10 16:31:29 +0000

    Adding create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, 0_WRONLY | 0_TRUNC | 0_CREAT, 0600);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
 
