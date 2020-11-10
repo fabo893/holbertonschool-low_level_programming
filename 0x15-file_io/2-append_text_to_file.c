eb3cb004e8ab7aa23a6f39252ecce196ba2e81af
fabo893 <jose.rosa893@gmail.com>
2020-11-10 15:50:30 +0000

    Adding append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,9 +19,9 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (len = 0; text_content[len] != '\0'; len++)
+		for (len = 0; text_content[len]; len++)
 		{
-			wr = write(fd, text_content, text_content[len]);
+			wr = write(fd, text_content, len);
 			if (wr == -1)
 				return (-1);
 		}
