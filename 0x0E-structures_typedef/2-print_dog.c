c7396054ff910bf136fa74670fca4bd54a232bf9
fabo893 <jose.rosa893@gmail.com>
2020-11-12 07:22:02 +0000

    Prototype to print a 'struct dog'

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,22 +6,18 @@
  */
 void print_dog(struct dog *d)
 {
-
-	if (d == NULL)
-		printf("nil");
-
-	if (d->name == NULL)
-		printf("Name: nil\n");
+	if (d->name == '\0')
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
 	if (d->age == 0)
-		printf("Age: nil\n");
+		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 
-	if (d->owner == NULL)
-		printf("Owner: nil\n");
+	if (d->owner == '\0')
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
