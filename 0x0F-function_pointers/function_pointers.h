5ba3ff42b5c93f2b30e7b3ee672fa59287327951
fabo893 <jose.rosa893@gmail.com>
2020-11-29 02:12:12 +0000

    adding prototype

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,8 +2,10 @@
 #define FUNCTION_POINTERS_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
