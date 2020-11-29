b926c6f28a51270fb24401068e99526648e237c9
fabo893 <jose.rosa893@gmail.com>
2020-11-29 03:15:32 +0000

    task #0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,14 +5,20 @@
  * @h: head of list of node fron which to start counting
  * Return: count of linked elements
  */
-size_t list_len(const list_t *h)
+size_t print_list(const list_t *h)
 {
-	size_t cnt = 0;
+	size_t nodes = 0;
 
+	if (!h)
+		return (0);
 	while (h != NULL)
 	{
-		cnt++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
 		h = h->next;
+		nodes++;
 	}
-	return (cnt);
+	return (nodes);
 }
