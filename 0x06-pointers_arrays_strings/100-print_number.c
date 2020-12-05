356f579c6948288a7e1a872e8cd67736888b1ad6
fabo893 <jose.rosa893@gmail.com>
2020-12-05 05:25:08 +0000

    Print number

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: number to be print.
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -n;
+	}
+	else
+	{
+		x = n;
+	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10 + 48);
+}
