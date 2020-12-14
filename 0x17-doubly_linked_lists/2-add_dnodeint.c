258999b80a075fa08895757507bfcc2ff00cdc6a
fabo893 <jose.rosa893@gmail.com>
2020-12-14 01:36:55 +0000

    add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,7 +17,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->prev = NULL;
 	new->n = n;
 	new->next = *head;
