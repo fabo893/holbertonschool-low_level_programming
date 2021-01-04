6ea441e96701b689e59924fb3e58627d357a2c53
fabo893 <jose.rosa893@gmail.com>
2021-01-04 18:43:39 +0000

    adding free 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - free linked list.
+ * @head: pointer of list.
+ * Return: nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	for (; *head != '\0';)
+	{
+		tmp = *head;
+		*head = tmp->next;
+		free(tmp);
+	}
+
+	*head = NULL;
+}
