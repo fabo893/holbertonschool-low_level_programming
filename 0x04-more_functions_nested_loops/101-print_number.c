89a6d77834e05815121a0f2602704fed369bb2dd
fabo893 <jose.rosa893@gmail.com>
2021-01-11 12:02:51 -0400

    printing the INT_MIN

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,21 +6,36 @@
   */
 void print_helper(int n)
 {
-	int idx, num, ls[15];
+        int idx, num, ls[18];
 
-	for (idx = 0; n != 0; idx++)
-	{
-		num = n % 10;
-		ls[idx] = num;
-		n = n / 10;
-	}
+        if (n == INT_MIN)
+        {
+                _putchar(50);
+                _putchar(49);
+                _putchar(52);
+                _putchar(55);
+                _putchar(52);
+                _putchar(56);
+                _putchar(51);
+                _putchar(54);
+                _putchar(52);
+                _putchar(56);
+        }
+        else
+        {
+                for (idx = 0; n != 0; idx++)
+                {
+                        num = n % 10;
+                        ls[idx] = num;
+                        n = n / 10;
+                }
 
-	idx--;
-	for (; idx >= 0; idx--)
-	{
-		_putchar(ls[idx] + '0');
-	}
-
+                idx--;
+                for (; idx >= 0; idx--)
+                {
+                        _putchar(ls[idx] + '0');
+                }
+        }
 }
 
 
@@ -30,15 +45,16 @@ void print_helper(int n)
   */
 void print_number(int n)
 {
-	if (n == 0)
-		_putchar('0');
+        if (n == 0)
+                _putchar('0');
 
-	if (n < 0)
-	{
-		_putchar(45);
-		n = (n * -1);
-	}
+        if (n < 0)
+        {
+                _putchar(45);
+                n = (n * -1);
+        }
 
-	print_helper(n);
+        print_helper(n);
 
 }
+
