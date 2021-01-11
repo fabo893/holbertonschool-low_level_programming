969ea682957225bfe600508d93d10f9aa6caa6fe
fabo893 <jose.rosa893@gmail.com>
2021-01-11 12:16:41 -0400

    another helper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,29 @@
 #include "holberton.h"
 
 /**
+  * print_int - print INT_MIN
+  * @n: number
+  */
+void print_int(int n)
+{
+	if (n == INT_MIN)
+	{
+		_putchar(50);
+		_putchar(49);
+		_putchar(52);
+		_putchar(55);
+		_putchar(52);
+		_putchar(56);
+		_putchar(51);
+		_putchar(54);
+		_putchar(52);
+		_putchar(56);
+	}
+}
+
+
+
+/**
   * print_helper - print the number given
   * @n: number
   */
@@ -8,34 +31,18 @@ void print_helper(int n)
 {
         int idx, num, ls[18];
 
-        if (n == INT_MIN)
-        {
-                _putchar(50);
-                _putchar(49);
-                _putchar(52);
-                _putchar(55);
-                _putchar(52);
-                _putchar(56);
-                _putchar(51);
-                _putchar(54);
-                _putchar(52);
-                _putchar(56);
-        }
-        else
-        {
-                for (idx = 0; n != 0; idx++)
-                {
-                        num = n % 10;
-                        ls[idx] = num;
-                        n = n / 10;
-                }
+	for (idx = 0; n != 0; idx++)
+	{
+		num = n % 10;
+		ls[idx] = num;
+		n = n / 10;
+	}
 
-                idx--;
-                for (; idx >= 0; idx--)
-                {
-                        _putchar(ls[idx] + '0');
-                }
-        }
+	idx--;
+	for (; idx >= 0; idx--)
+	{
+		_putchar(ls[idx] + '0');
+	}
 }
 
 
@@ -45,6 +52,7 @@ void print_helper(int n)
   */
 void print_number(int n)
 {
+
         if (n == 0)
                 _putchar('0');
 
@@ -53,8 +61,12 @@ void print_number(int n)
                 _putchar(45);
                 n = (n * -1);
         }
-
-        print_helper(n);
+	if (n == INT_MIN)
+	{
+		print_int(n);
+	}
+	else
+        	print_helper(n);
 
 }
 
