cd373d42f322b33fa8d92396b1a3bb11a7f5c023
fabo893 <jose.rosa893@gmail.com>
2021-01-23 00:24:48 +0000

    putting the unsigned long int type

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table_t *table;
 
-	table = malloc(sizeof(hash_table_t) * size);
+	table = malloc(sizeof(unsigned long int) * size);
 	if (table == NULL)
 		return (NULL);
 
