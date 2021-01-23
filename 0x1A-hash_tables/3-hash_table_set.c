eb0a3214e7bc9fe915fb479a388e5f72862f8709
fabo893 <jose.rosa893@gmail.com>
2021-01-23 23:13:15 +0000

    fixing the compare signs

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,17 +14,33 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	int idx;
 	unsigned long int kidx;
 	hash_node_t *node;
+	hash_node_t *old;
 
 	for (idx = 0; key[idx] != '\0'; idx++)
 		;
 	if (idx == 0)
 		return (0);
+	if (ht == NULL)
+		return(0);
+
+	kidx = key_index((unsigned char *)key, (*ht).size);
+	if ((*ht).array[kidx] != NULL)
+	{
+		old = (*ht).array[kidx];
+		for (idx = 0; old != NULL; idx++)
+		{
+			if ((*old).key == key)
+			{
+				if ((*old).value == value)
+					return (1);
+			}
+			old = (*old).next;
+		}
+	}
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
-
-	kidx = key_index((unsigned char *)key, (*ht).size);
 
 	(*node).key = (char *)key;
 	(*node).value = (char *)value;
