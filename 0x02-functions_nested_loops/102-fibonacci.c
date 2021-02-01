fe9c4e857365b6be14d3a32619835d6413e749bb
fabo893 <jose.rosa893@gmail.com>
2021-02-01 03:30:33 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main program
+ *
+ * Return: On success, always 0
+ */
+int main(void)
+{
+	unsigned int long i, x, res, tmp;
+
+	tmp = 1;
+	x = 2;
+	printf("%lu, %lu, ", tmp, x);
+	for (i = 1; i < 49; i++)
+	{
+		if (i > 1)
+			printf(", ");
+		res = tmp + x;
+		printf("%lu", res);
+		tmp = x;
+		x = res;
+	}
+	printf("\n");
+
+	return (0);
+}
