71381b558e3e8339e130e7ecce6c558386145f5e
fabo893 <jose.rosa893@gmail.com>
2021-04-20 01:49:28 +0000

    ReadMe file, linear search, and header file

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search for a value in an array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: On success, first index where value is located
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i, x;
+
+	x = size;
+	for (i = 0; i < x; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* LINEAR SEARCH PROTOTYPE */
+int linear_search(int *array, size_t size, int value);
+
+/* BINARY SEARCH PROTOTYPE */
+int binary_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
